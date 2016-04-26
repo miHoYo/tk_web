274f8a8a3d9c2d5cf8cbcd2adfa548ae0ca5ccbb
miHoYo <li413279740@qq.com>
2016-04-26 21:48:55 +0800

    weixin
    微信项目

---
+++ Application/Common/Conf/config.php
@@ -0,0 +1,15 @@
+<?php
+return array(
+	//'配置项'=>'配置值'
+		'DEFAULT_MODULE'        =>  'WeiXin',  // 默认模块
+		'DEFAULT_CONTROLLER'    =>  'Index', // 默认控制器名称
+		'DEFAULT_ACTION'        =>  'index', // 默认操作名称
+		'MODULE_ALLOW_LIST'     =>  array('Home','WeiXin'),
+		'DB_TYPE'               =>  'Mysql',     // 数据库类型
+		'DB_HOST'               =>  '103.1.224.125', // 服务器地址
+		'DB_NAME'               =>  'a0304114132',          // 数据库名
+		'DB_USER'               =>  'a0304114132',      // 用户名
+		'DB_PWD'                =>  '491e7c30',          // 密码
+		'DB_PORT'               =>  '3306',        // 端口
+		'DB_PREFIX'             =>  '',    // 数据库表前缀
+);
---
+++ Application/WeiXin/Controller/IndexController.class.php
@@ -0,0 +1,37 @@
+<?php
+namespace WinXin\Controller;
+use Think\Controller;
+
+define("TOKEN", "xcoderstudio");
+
+
+class IndexController extends Controller {
+	public function index() {
+	        $echoStr = $_GET["echostr"];
+	 
+	        //valid signature , option
+	        if ($this->checkSignature()) {
+	            echo $echoStr;
+	            exit;
+	        }
+	    }
+ 
+    private function checkSignature() {
+        $signature = $_GET["signature"];
+        $timestamp = $_GET["timestamp"];
+        $nonce = $_GET["nonce"];
+ 
+        $token = TOKEN;
+        $tmpArr = array($token, $timestamp, $nonce);
+        sort($tmpArr, SORT_STRING);
+        $tmpStr = implode($tmpArr);
+        $tmpStr = sha1($tmpStr);
+ 
+        if ($tmpStr == $signature) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+	
+}
