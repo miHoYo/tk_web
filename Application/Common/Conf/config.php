0d1b1eb456c4bed8f91151180d5bcb197bef7278
timxinyao <348589625@qq.com>
2016-04-27 14:12:04 +0800

    微信模块重写

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -1,10 +1,10 @@
 <?php
 return array(
 	//'配置项'=>'配置值'
-		'DEFAULT_MODULE'        =>  'WeiXin',  // 默认模块
+		'DEFAULT_MODULE'        =>  'Weixin',  // 默认模块
 		'DEFAULT_CONTROLLER'    =>  'Index', // 默认控制器名称
 		'DEFAULT_ACTION'        =>  'index', // 默认操作名称
-		'MODULE_ALLOW_LIST'     =>  array('Home','WeiXin'),
+		'MODULE_ALLOW_LIST'     =>  array('Weixin','Home'),
 		'DB_TYPE'               =>  'Mysql',     // 数据库类型
 		'DB_HOST'               =>  '103.1.224.125', // 服务器地址
 		'DB_NAME'               =>  'a0304114132',          // 数据库名
